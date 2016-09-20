88b019a3ffd83a9298be3cb1458fcdd9ddf3c251
yulyazurdia8 <yulyazurdia8@hotmail.com>
2016-09-19 22:53:43 -0600

    Recursive functions. Resolution for prime numbers and palindromic words.

---
+++ RecursividadEjercicios/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolucionParcial1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Recursividad ejercicios = new Recursividad();
+            
+            string palabra = "";
+            Console.WriteLine("Ingrese una palabra: ");
+            palabra = Console.ReadLine();
+
+            Console.WriteLine(ejercicios.Pal(palabra));
+
+            Console.WriteLine(ejercicios.Primo(5));
+            Console.WriteLine(ejercicios.Primo(6));
+            Console.ReadLine();
+        }     
+    }
+}
