65c7b8deb284205aedcb813944b8d2fb91835a94
yulyazurdia8 <yulyazurdia8@hotmail.com>
2016-09-20 19:40:04 -0600

    Changes on the program, so user can enter the information

--- RecursividadEjercicios/Program.cs
+++ RecursividadEjercicios/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ResolucionParcial1
+namespace EjerciciosdeRecursividad
 {
     class Program
     {
@@ -12,15 +12,37 @@ namespace ResolucionParcial1
         static void Main(string[] args)
         {
             Recursividad ejercicios = new Recursividad();
-            
+
             string palabra = "";
+            int numero = 0;
+
+            Console.WriteLine("PALÍNDROMOS: ");
             Console.WriteLine("Ingrese una palabra: ");
             palabra = Console.ReadLine();
 
-            Console.WriteLine(ejercicios.Pal(palabra));
+            if (ejercicios.Pal(palabra) == true)
+            {
+                Console.WriteLine("La palabra [" + palabra + "] es palíndromo");
+            }
+            else
+            {
+                Console.WriteLine("La palabra [" + palabra + "] no es palíndromo");
+            }
 
-            Console.WriteLine(ejercicios.Primo(5));
-            Console.WriteLine(ejercicios.Primo(6));
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("NÚMEROS PRIMOS: ");
+            Console.WriteLine("Ingrese un número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (ejercicios.Primo(numero) == true)
+            {
+                Console.WriteLine("El número [" + numero + "] es primo");
+            }
+            else
+            {
+                Console.WriteLine("El número [" + numero + "] no es primo");
+            }
+
             Console.ReadLine();
         }     
     }
